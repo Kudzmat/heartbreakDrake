7dc173a24271d48d61b493b5e48a92fe8f1f8991
Kudz <kudzayibamhare@gmail.com>
2023-09-14 22:21:55 +0200

    created functions for generating word cloud and graphs

--- clean_data.py
+++ clean_data.py
@@ -8,41 +8,38 @@ import nltk
 from nltk.corpus import stopwords
 from collections import Counter
 from nltk.tokenize import word_tokenize
-import string
-import pprint
+
 
 nltk.download('punkt')
 nltk.download('stopwords')
 
 DRAKE_DATA = pd.read_csv('drake/drake_data.csv')
 TINASHE_DATA = pd.read_csv('tinashe/tinashe.csv')
+ALBUM = 'Take Care'
 
 
 # Get album lyrics from user input
 def get_album_lyrics():
     album_lyrics = []  # empty list will hold all the lyrics from an album by track
 
-    for index, row in TINASHE_DATA.iterrows():
-        lyrics = row['lyrics']
-        album_lyrics.append(lyrics)
-
-    """"
     for index, row in DRAKE_DATA.iterrows():
-        if row['album'] == 'Take Care':
+        if row['album'] == ALBUM:
             lyrics = row['lyrics']
             album_lyrics.append(lyrics)
-    """
+
     return album_lyrics
 
 
 # Processing the lyrics by cleaning the up
 def process_lyrics(lyrics_list):
     processed = []
+    custom_stopwords = ['yeah', 'oh', 'nigga', 'chorus', 'Drake', 'drake', 'niggas']
     for lyrics in lyrics_list:
         new_lyrics = lyrics.lower()
         tokens = word_tokenize(new_lyrics)
-        # Remove punctuation and stopwords
-        tokens = [word for word in tokens if word.isalpha() and word not in stopwords.words('english')]
+        # Remove punctuation and stopwords (both English and custom)
+        tokens = [word for word in tokens if word.isalpha() and word.lower() not in stopwords.words(
+            'english') and word.lower() not in custom_stopwords]
         processed.extend(tokens)  # Extend the list with the tokens
     return processed
 
@@ -51,12 +48,12 @@ def check_frequency(tokens):
     lyrics_count = {}  # This dictionary will hold a word and the number of times it appears
     word_frequency = Counter(tokens)
     # Find the 10 most common words
-    most_common_words = dict(word_frequency.most_common(50))
+    most_common_words = dict(word_frequency.most_common(20))
 
     # Print the most common words and their frequencies
     for word, num in most_common_words.items():
         lyrics_count[word] = num
-        #print(f'{word}: {num}')
+        # print(f'{word}: {num}')
 
     return lyrics_count
 
@@ -69,9 +66,39 @@ def create_bar_graph(count_dict):
     plt.show()
 
 
+def create_word_cloud(words):
+    # Combine the words into a single string
+    text = " ".join([word for word in words.keys()])
+
+    # Generate the word cloud
+    word_cloud = WordCloud(max_font_size=50, max_words=100, background_color="white").generate_from_frequencies(words)
+    plt.figure()
+    # This is to make the displayed image appear more smoothly
+    plt.imshow(word_cloud, interpolation="bilinear")
+    plt.axis("off")
+    plt.show()
+
+
+def create_bar_chart(word_freq_dict, top_n=10):
+    # Sort the word frequency dictionary by values in descending order
+    sorted_word_freq = sorted(word_freq_dict.items(), key=lambda x: x[1], reverse=True)
+
+    # Extract the top N words and their frequencies
+    top_words = [item[0] for item in sorted_word_freq[:top_n]]
+    top_freqs = [item[1] for item in sorted_word_freq[:top_n]]
+
+    # Create a bar chart
+    plt.figure(figsize=(10, 6))
+    plt.bar(top_words, top_freqs)
+    plt.xlabel('Words')
+    plt.ylabel('Frequency')
+    plt.title('Top {} Words'.format(top_n))
+    plt.xticks(rotation=45)
+    plt.show()
+
+
 lyrics_list = get_album_lyrics()
 clean_lyrics = process_lyrics(lyrics_list)
 
 word_count = check_frequency(clean_lyrics)
-create_bar_graph(word_count)
-
+create_word_cloud(word_count)
